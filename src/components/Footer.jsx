eeeb57241c4575b0c32faa6d4d202baef7a465bf
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-28 09:11:38 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 mt-8">
+      <div className="container mx-auto text-center text-white">
+        <p>© 2024 Kuntul. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
