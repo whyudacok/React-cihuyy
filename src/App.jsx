df3a9ebef1f040633b5d9619ff1b380417fc5ccf
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-28 09:13:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Anime from './pages/Anime';
+import Manga from './pages/Manga';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/anime" element={<Anime />} />
+          <Route path="/manga" element={<Manga />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
