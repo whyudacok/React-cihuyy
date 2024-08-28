a5785bacc016a2b98aa0801a494ebd2fc3e9e8cd
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-29 02:07:49 +0700

    Update Manga.jsx

--- src/pages/Manga.jsx
+++ src/pages/Manga.jsx
@@ -1,9 +1,9 @@
-// src/pages/Manga.js
 import React from 'react';
+import LatestAnime from './components/LatestAnime'; // Import komponen
 
 function Manga() {
   return (
-    <div className="flex flex-row transition-all duration-300 ease-in-out">
+<div className="flex flex-row transition-all duration-300 ease-in-out">
   <div className="w-screen max-w-full overflow-hidden lg:pl-16">
     <div className="relative flex items-center justify-center flex-col">
       <div className="absolute h-[101%] w-full bg-gradient-to-r from-black from-0% to-transparent to-50%" />
@@ -395,96 +395,7 @@ function Manga() {
       </div>
       <div />
       <div>
-        <div className="scroll-smooth flex flex-col mx-auto my-1 md:my-0">
-          <div className="flex items-center gap-2 px-3 xl:px-0">
-            <span className="w-[0.35rem] h-6 md:w-[0.3rem] rounded-md bg-white" />
-            <h1 className="text-[19px] sm:text-[22px] my-4 font-medium md:font-semibold">
-              Anime update
-            </h1>
-          </div>
-          <div className="relative">
-            <span className="flex items-center mb-5 cursor-pointer absolute left-0 bg-gradient-to-r from-black z-10 transition-all duration-300 invisible opacity-0 h-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={28}
-                height={28}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mb-4"
-              >
-                <path d="m15 18-6-6 6-6" />
-              </svg>
-            </span>
-            <span className="flex items-center mb-5 cursor-pointer absolute right-0 bg-gradient-to-l from-black z-10 transition-all duration-300 lg:px-3 visible h-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={28}
-                height={28}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mb-4"
-              >
-                <path d="m9 18 6-6-6-6" />
-              </svg>
-            </span>
-            <div
-              className="relative flex flex-nowrap overflow-scroll overflow-y-hidden gap-3 scrollbar-hide pl-[0.75rem] xl:pl-0"
-              id="Popular This Season"
-              style={{ cursor: "auto" }}
-            >
-              <a
-                href="/anime/koi-futago-warikirenai-sub-indo/"
-                style={{ cursor: "pointer" }}
-              >
-                <div className="relative flex flex-col h-full hover:cursor-pointer group w-[105px] sm:w-[135px] md:w-[155px] xl:w-[175px]">
-                  <div className="flex-shrink-0 absolute top-0 right-0 flex font-medium items-center justify-center gap-[.4rem] bg-black/60 backdrop-blur text-white !text-xs line-clamp-1 z-[7] px-2 py-1 rounded-bl-lg tracking-wider">
-                    <span className="hidden md:flex">Episode</span>
-                    <span className="md:hidden">Ep</span>{" "}
-                    <span className="font-medium text-purple-400">77</span>
-                  </div>
-                  <div className="relative h-[160px] w-[105px] sm:w-[135px] sm:h-[190px] md:h-[230px] md:w-[155px] xl:h-[255px] xl:w-[175px] rounded-xl xl:rounded-2xl">
-                    <div className="w-full h-full rounded-xl xl:rounded-2xl overflow-hidden bg-[#1e1e24] aspect-[15/9] flex-shrink-0 shadow-[4px_0px_5px_0px_rgba(0,0,0,0.3)] group">
-                      <img
-                        alt="[Oshi no Ko] 2nd Season"
-                        loading="eager"
-                        width={155}
-                        height={230}
-                        decoding="async"
-                        data-nimg={1}
-                        className="w-full h-full object-cover rounded-xl xl:rounded-2xl transition-transform duration-300 group-hover:scale-105"
-                        style={{ color: "transparent" }}
-                        src="https://otakudesu.cloud/wp-content/uploads/2024/07/142250.jpg"
-                      />
-                    </div>
-                    <div className="w-full h-full rounded absolute group-hover:bg-gradient-to-t from-black/85 to-transparent opacity-0 group-hover:opacity-100 top-0 z-[5] transition-all duration-300 ease justify-center">
-                      <div className="bottom-4 left-0 right-0 absolute text-xs font-medium flex flex-wrap items-center justify-center gap-[.3rem] z-[7]">
-                        <span className="uppercase">Ep 77</span>{" "}
-                        <span className="text-[10px]">•</span>
-                        <span className="font-semibold text-green-400">
-                          Kamis
-                        </span>
-                        <span className="text-[10px]">•</span>
-                        <span className="">23 Agu</span>
-                      </div>
-                    </div>
-                  </div>
-                  <span className="overflow-hidden text-center text-[#d1d7e0] pt-1.5 px-1.5 sm:px-2 text-xs sm:text-sm font-medium line-clamp-2">
-                    <span className="aspect-square w-2 h-2 inline-block mr-1 rounded-full bg-green-500 xl:hidden" />{" "}
-                    Koi wa Futago de Warikirenai
-                  </span>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
+       <LatestAnime/>
       </div>
       <div className="lg:flex lg:flex-row justify-between lg:gap-20">
         <div
@@ -660,7 +571,7 @@ function Manga() {
     </div>
   </div>
 </div>
-  );
-}
-
-export default Manga;
+);
+    }
+    
+    export default Manga;
