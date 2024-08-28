b3be9c87ac4c5d5f80f1f36001e2e19d70c9ae5d
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-28 08:59:44 +0700

    Delete src directory

--- src/arrayLength.js
+++
@@ -1,7 +0,0 @@
-export function arrayLength(count) {
-  let array = []
-  for (let i = 1; i <= count; i++) {
-    array.push(i)
-  }
-  return array
-}
--- src/components/FootList.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function FootList({ children }) {
-  return (
-    <>
-      <li className='text-sm lg:text-base hover:text-white'>{children}</li>
-    </>
-  )
-}
